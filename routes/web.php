f97be8407ecf75b26d10c627e309930f78ac70dc
AndyLinxies <linxies@gmail.com>
2021-07-19 11:14:14 +0200

    Exo1 Serie1

---
+++ routes/web.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/andy', function () {
+    $nom= '<h2>Je me nomme andy</h2>';
+    return $nom;
+});
+
+
